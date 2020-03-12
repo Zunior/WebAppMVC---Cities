4c697a105924e92253df323227a28ad2659ff740
Zunior <sasa.popovic.prog@gmail.com>
2020-03-12 10:48:07 +0100

    # WARNING: head commit changed in the meantime
    master

--- src/constants/PageConstants.java
+++ src/constants/PageConstants.java
@@ -1,7 +1,7 @@
 package constants;
 
 public interface PageConstants {
-	public static final String PAGE_ADD_CITY = "/WEB-INF/DodajNoviGrad.jsp";
+	public static final String PAGE_ADD_CITY = "/WEB-INF/pages/DodajNoviGrad.jsp";
 	public static final String PAGE_DELETE_CITY = "../WEB-INF/pages/ListaGradova.jsp";
 	public static final String PAGE_EDIT_CITY = "../WEB-INF/pages/Edit.jsp";
 	public static final String PAGE_LIST_CITY = "/WEB-INF/pages/ListaGradova.jsp";
