5db12cab8a8c06ff517e81f2b58f909198922f6f
Zunior <sasa.popovic.prog@gmail.com>
2020-03-12 11:54:55 +0100

    ide

--- src/action/impl/DodajAction.java
+++ src/action/impl/DodajAction.java
@@ -1,17 +1,41 @@
 package action.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import action.AbstractAction;
 import constants.ViewConstants;
+import domain.City;
+import entity.Place;
+import servis.PlaceServis;
+import servis.servisImpl.PlaceServisImpl;
 
 public class DodajAction extends AbstractAction{
+	private final PlaceServis placeServis;
+
+	public DodajAction() {
+		placeServis = new PlaceServisImpl();
+	}
 
 	@Override
 	public String execute(HttpServletRequest request) {
+		List<City> cities = (List<City>) request.getServletContext().getAttribute("cities");
+		if(cities == null)
+			cities = new ArrayList<City>();
+		City city = new City(56000, "Kragujevac");
+		
+		int brojGrada = Integer.parseInt((String) request.getParameter("brojGrada"));
+		String imeGrada = (String) request.getParameter("imeGrada");
+		cities.add(new City(brojGrada, imeGrada));
+		
+		//save city in database
+//		Place place = new Place(city.getCityNumber(), city.getNaziv());
 		
 //		return "/WEB-INF/DodajNoviGrad.jsp";
 		return ViewConstants.ADD_CITY;
 	}
+	
 	
 }
